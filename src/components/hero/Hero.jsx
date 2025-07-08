f838c937768530a6bc2fece23577acc462a50b49
Alan <83542792+gurhanalan@users.noreply.github.com>
2025-07-08 12:51:14 -0500

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,23 +6,9 @@ const Hero = () => {
     <section id='welcome' className={styles.welcome}>
       <div className={styles.snow}></div>
       <div className={styles['text-box']}>
-        <h1>Gurhan Alan</h1>
-        <TypeAnimation
-          cursor={true}
-          sequence={['Frontend Developer', 2000, 'React Developer', 2000]}
-          wrapper='p'
-          repeat={Infinity}
-        />
-        {/* <p>Frontend Developer</p> */}
-        <p className={styles.small}>and also,</p>
-        <div className={styles.title}>
-          <div className={styles.title__wrapper}>
-            <div className={styles.title__item}>Captain</div>
-            <div className={styles.title__item}>Pilot</div>
-            <div className={styles.title__item}>Ex-Officer </div>
-            <div className={styles.title__item}>Project Manager</div>
-            <div className={styles.title__item}>Arg. Tango Instructor</div>
-          </div>
+        <h1>Alan</h1>
+
+        
         </div>
       </div>
     </section>
